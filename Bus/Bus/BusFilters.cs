f2a5f5f3e09b980ce2d84752e99aeba294503508
Svveetcandy <73545335+Svveetcandy@users.noreply.github.com>
2020-11-10 12:02:09 -0200

    Add files via upload

---
+++ Bus/Bus/BusFilters.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class BusFilters
+    {
+        public List<Bus> marshNum(List<Bus> allBuses)
+        {
+            List<Bus> allFilteredBuses = new List<Bus>();
+            Console.Write("Для фильтрации ведите номер маршрута: ");
+            int marshnum = int.Parse(Console.ReadLine());
+            foreach (Bus bus in allBuses)
+            {
+                if (bus.marshNum == marshnum)
+                {
+                    allFilteredBuses.Add(bus);
+                }
+            }
+            return allFilteredBuses;
+        }
+        public List<Bus> year(List<Bus> allBuses)
+        {
+            List<Bus> allFilteredBuses = new List<Bus>();
+            Console.Write("Для фильтрации введите количество лет эксплуатции: ");
+            int year = int.Parse(Console.ReadLine());
+            int todayYear = DateTime.Now.Year;
+            Console.Write("Как отфильтровать?\n1.Больше n лет.\n2.Равно n лет.\n3.Меньше n лет.\n");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    foreach (Bus bus in allBuses)
+                    {
+                        if (todayYear - bus.year > year)
+                        {
+                            allFilteredBuses.Add(bus);
+                        }
+                    }
+                    break;
+                case "2":
+                    foreach (Bus bus in allBuses)
+                    {
+                        if (todayYear - bus.year == year)
+                        {
+                            allFilteredBuses.Add(bus);
+                        }
+                    }
+                    break;
+                case "3":
+                    foreach (Bus bus in allBuses)
+                    {
+                        if (todayYear - bus.year < year)
+                        {
+                            allFilteredBuses.Add(bus);
+                        }
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Было введено недопустимое значение.");
+                    break;
+            }
+
+            return allFilteredBuses;
+        }
+        public List<Bus> probeg(List<Bus> allBuses)
+        {
+            List<Bus> allFilteredBuses = new List<Bus>();
+            Console.Write("Для фильтрации ведите пробег автобуса: ");
+            int probeg = int.Parse(Console.ReadLine());
+            Console.Write("Как отфильтровать?\n1.Больше n километров.\n2.Равно n километров.\n3.Меньше n километров.\n");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    foreach (Bus bus in allBuses)
+                    {
+                        if (bus.probeg > probeg)
+                        {
+                            allFilteredBuses.Add(bus);
+                        }
+                    }
+                    break;
+                case "2":
+                    foreach (Bus bus in allBuses)
+                    {
+                        if (bus.probeg == probeg)
+                        {
+                            allFilteredBuses.Add(bus);
+                        }
+                    }
+                    break;
+                case "3":
+                    foreach (Bus bus in allBuses)
+                    {
+                        if (bus.probeg < probeg)
+                        {
+                            allFilteredBuses.Add(bus);
+                        }
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Было введено недопустимое значение.");
+                    break;
+            }
+
+            return allFilteredBuses;
+        }
+        public List<Bus> marka(List<Bus> allBuses)
+        {
+            List<Bus> allFilteredBuses = new List<Bus>();
+            Console.Write("Для фильтрации ведите марку автобуса: ");
+            string marka = Console.ReadLine();
+            foreach (Bus bus in allBuses)
+            {
+                if (bus.marka == marka)
+                {
+                    allFilteredBuses.Add(bus);
+                }
+            }
+            return allFilteredBuses;
+        }
+    }
+}
---
+++ Bus/Bus/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool escape = false;
+            List<Bus> buses = new List<Bus>();
+            List<Bus> filteredBuses = new List<Bus>();
+            BusFilters filter = new BusFilters();
+            while (!escape)
+            {
+                Console.Clear();
+                Console.Write("1.Добавить автобус в список.\n2.Фильтр по маршруту.\n3.Фильтр по эксплуатации\n4.Фильтр по пробегу\n5. Фильтр по марке.\n6. Выход.\n");
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        buses.Add(new Bus());
+                        break;
+                    case "2":
+                        filteredBuses.Clear();
+                        filteredBuses = filter.marshNum(buses);
+                        foreach (Bus bus in filteredBuses)
+                        {
+                            bus.Print();
+                        }
+                        break;
+                    case "3":
+                        filteredBuses.Clear();
+                        filteredBuses = filter.year(buses);
+                        foreach (Bus bus in filteredBuses)
+                        {
+                            bus.Print();
+                        }
+                        break;
+                    case "4":
+                        filteredBuses.Clear();
+                        filteredBuses = filter.probeg(buses);
+                        foreach (Bus bus in filteredBuses)
+                        {
+                            bus.Print();
+                        }
+                        break;
+                    case "5":
+                        filteredBuses.Clear();
+                        filteredBuses = filter.marka(buses);
+                        foreach (Bus bus in filteredBuses)
+                        {
+                            bus.Print();
+                        }
+                        break;
+                    case "6":
+                        escape = true;
+                        break;
+                    default:
+                        Console.WriteLine("Было введено недопустимое значение.");
+                        break;
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
